7faa9fa31bb0929fb05f54066a6adfd09078b5be
BaraaAlArab <baraa9000alarab@gmail.com>
2025-05-05 19:34:57 +0300

    create an api to get all data from database.

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,16 @@ dotenv.config();
 const app = express();
 app.use(express.json()); // Accept JSON data
 
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({success: true, data: products});
+  } catch (error) {
+    console.log("error in fetching products:", error.message);
+    res.status(500).json({success:false, message:"servererror"})
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   const {name, price, image} = req.body;
 
