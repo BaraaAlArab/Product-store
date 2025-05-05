7993ed2ea1427ae257900aedee2ac14273cf0f7e
BaraaAlArab <baraa9000alarab@gmail.com>
2025-05-05 23:09:20 +0300

    finished the backend.

---
+++ backend/Controller/Product.controller.js
@@ -0,0 +1,56 @@
+import Product from "../models/Product.model.js";
+import mongoose from "mongoose";
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({success: true, data: products});
+  } catch (error) {
+    console.log("error in fetching products:", error.message);
+    res.status(500).json({success: false, message: "servererror"});
+  }
+};
+export const createProduct = async (req, res) => {
+  const {name, price, image} = req.body;
+
+  if (!name || !price || !image) {
+    return res
+      .status(400)
+      .json({success: false, message: "Please provide all fields"});
+  }
+
+  const newProduct = new Product({name, price, image});
+
+  try {
+    await newProduct.save();
+    res.status(201).json({success: true, data: newProduct});
+  } catch (error) {
+    console.error("Error creating product:", error.message);
+    res.status(500).json({success: false, message: "Server Error"});
+  }
+};
+export const updateProduct = async (req, res) => {
+  const {id} = req.params;
+  const products = req.body;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({success: false, message: "no product found with that id..."});
+  }
+  try {
+    const UpdatedProduct = await Product.findByIdAndUpdate(id, products, {
+      new: true,
+    });
+    res.status(200).json({success: true, data: UpdatedProduct});
+  } catch (error) {
+    res.status(500).json({success: false, message: "Server error"});
+  }
+};
+export const deleteProduct = async (req, res) => {
+  const {id} = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({success: true, message: "Product deleted"});
+  } catch (error) {
+    res.status(404).json({success: false, message: "product not found"});
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import {connectDB} from "./config/db.js";
-import Product from "./models/Product.model.js"; // Capitalized
+import products from "./Routes/Product.Routes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json()); // Accept JSON data
 
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json({success: true, data: products});
-  } catch (error) {
-    console.log("error in fetching products:", error.message);
-    res.status(500).json({success:false, message:"servererror"})
-  }
-});
-
-app.post("/api/products", async (req, res) => {
-  const {name, price, image} = req.body;
-
-  if (!name || !price || !image) {
-    return res
-      .status(400)
-      .json({success: false, message: "Please provide all fields"});
-  }
-
-  const newProduct = new Product({name, price, image});
-
-  try {
-    await newProduct.save();
-    res.status(201).json({success: true, data: newProduct});
-  } catch (error) {
-    console.error("Error creating product:", error.message);
-    res.status(500).json({success: false, message: "Server Error"});
-  }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-  const {id} = req.params;
-  try {
-    await Product.findByIdAndDelete(id);
-    res.status(200).json({success: true, message: "Product deleted"});
-  } catch (error) {
-    res.status(404).json({success: false, message: "product not found"});
-  }
-});
+app.use("/api/products", products);
 app.listen(5000, () => {
   connectDB();
   console.log("server started at http://localhost:5000 ");
