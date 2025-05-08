5793afb7d5a906d63b8fdd3cdb70ebddee71e102
BaraaAlArab <baraa9000alarab@gmail.com>
2025-05-08 13:10:29 +0300

    install Vite and chakra UI.

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,13 @@ import {connectDB} from "./config/db.js";
 import products from "./Routes/Product.Routes.js";
 
 dotenv.config();
-
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json()); // Accept JSON data
 
 app.use("/api/products", products);
-app.listen(5000, () => {
+
+app.listen(PORT, () => {
   connectDB();
-  console.log("server started at http://localhost:5000 ");
+  console.log("server started at http://localhost: " + PORT);
 });
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import {Button} from "./components/ui/Button.jsx";
+function App() {
+  return (
+    <>
+      <Button>Hello</Button>
+    </>
+  );
+}
+
+export default App;
