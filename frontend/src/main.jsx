980881806d8c628a48440e2c0239ada082ea9474
BaraaAlArab <baraa9000alarab@gmail.com>
2025-06-10 16:42:54 +0300

    added a toast message

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,13 @@ import {Provider} from "./components/ui/provider";
 import App from "./App.jsx";
 
 import {BrowserRouter} from "react-router-dom";
+import {Toaster} from "./components/ui/toaster";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider>
         <App />
+        <Toaster />
       </Provider>
     </BrowserRouter>
   </StrictMode>,
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,4 +1,5 @@
 import {Box, Button, Container, Heading, Input, VStack} from "@chakra-ui/react";
+import {toaster} from "../components/ui/toaster.jsx";
 import {useColorModeValue} from "../components/ui/color-mode";
 
 import {useState} from "react";
@@ -10,22 +11,43 @@ function CreatePage() {
     price: "",
     image: "",
   });
+
   const {createProduct} = useProductStore();
 
   const handleAddProduct = async () => {
     const {success, message} = await createProduct(newProduct);
-    console.log("Success:", success);
-    console.log("Message:", message);
+
+    if (success) {
+      toaster.create({
+        title: "Product created.",
+        description: message || "Your product was successfully added.",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+      // Optionally clear form
+      setNewProduct({name: "", price: "", image: ""});
+    } else {
+      toaster.create({
+        title: "Error creating product.",
+        description: message || "Something went wrong.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+      setNewProduct({name: "", price: "", image: ""});
+    }
   };
+
   return (
     <Container maxW={"container.sm"}>
       <VStack spacing={8}>
-        <Heading as={"h1"} size={"2x1"} textAlign={"center"} mb={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
           Create new Product
         </Heading>
         <Box
           w={"full"}
-          bg={useColorModeValue("White", "gray.800")}
+          bg={useColorModeValue("white", "gray.800")}
           p={6}
           rounded={"lg"}
           shadow={"md"}
